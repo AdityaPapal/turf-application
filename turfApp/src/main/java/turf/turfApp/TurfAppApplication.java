571b3aa708f8bf7c7516360ec229b72bd8abcf1d
Ashutosh <ashutosh10official@gmail.com>
2024-06-22 21:00:39 +0530

    java,springboot version change

--- turfApp/src/main/java/turf/turfApp/TurfAppApplication.java
+++ turfApp/src/main/java/turf/turfApp/TurfAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TurfAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hi all");
 		SpringApplication.run(TurfAppApplication.class, args);
 	}
 
