a41b2f9c1602407c41c6d215576a67122eb313e9
Ashutosh <ashutosh10official@gmail.com>
2024-06-22 20:29:21 +0530

    initial setup

---
+++ turfApp/src/main/java/turf/turfApp/TurfAppApplication.java
@@ -0,0 +1,13 @@
+package turf.turfApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurfAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurfAppApplication.class, args);
+	}
+
+}
---
+++ turfApp/src/test/java/turf/turfApp/TurfAppApplicationTests.java
@@ -0,0 +1,13 @@
+package turf.turfApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurfAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
